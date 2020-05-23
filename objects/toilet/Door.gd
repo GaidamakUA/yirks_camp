cfa2f660e989b5b51d2331dbf7ba8f2e8c18d968
Yurii Mazurevych <jmazurevych@gmail.com>
2020-05-23 10:24:00 +0300

    Added palm to indicate interaction

--- objects/toilet/Door.gd
+++ objects/toilet/Door.gd
@@ -2,6 +2,7 @@ extends StaticBody2D
 
 onready var animation_player = $AnimationPlayer
 onready var collision_shape = $CollisionShape2D
+onready var yeerk_palm = $YeerkPalm
 
 func open():
 	animation_player.play("Open")
@@ -11,3 +12,9 @@ func close():
 
 func is_open() -> bool:
 	return collision_shape.disabled
+
+func show_palm():
+	yeerk_palm.show()
+
+func hide_palm():
+	yeerk_palm.hide()
--- objects/toilet/Toilet.gd
+++ objects/toilet/Toilet.gd
@@ -7,3 +7,9 @@ func _interact():
 		door.close()
 	else:
 		door.open()
+
+func _on_yeerk_entered(area):
+	door.show_palm()
+
+func _on_yeerk_exited(area):
+	door.hide_palm()
