86f59ed1a340d601a656ec19d3859c821d102b15
Yurii Mazurevych <jmazurevych@gmail.com>
2020-11-29 13:51:12 +0200

    Added icons for MacOS and Windows

--- objects/bus/control_panel/BaseInteractiveArea.gd
+++ objects/bus/control_panel/BaseInteractiveArea.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-class_name BaseClickableArea
+class_name BaseInteractiveArea
 
 func _on_BaseInteractiveArea_input_event(viewport, event, shape_idx):
 	if not event is InputEventMouseButton:
@@ -8,14 +8,14 @@ func _on_BaseInteractiveArea_input_event(viewport, event, shape_idx):
 	if not event.button_index == BUTTON_LEFT:
 		return
 	if event.pressed:
-		_on_pressed()
+		on_pressed()
 	else:
-		_on_released()
+		on_released()
 
 # To be overriden by children
-func _on_pressed():
+func on_pressed():
 	pass
 
 # To be overriden by children
-func _on_released():
+func on_released():
 	pass
